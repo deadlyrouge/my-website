dfd45b2cf747b653177e55c1ec68a0dd1a5c948d
deadlyrouge <75321791+deadlyrouge@users.noreply.github.com>
2020-12-10 21:11:58 +0530

    Add files via upload

---
+++ precache-manifest.9e154d0792a6ab4aa13e43315bd00797.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "47053f26b4788b9f2a4d",
+    "url": "/static/js/app.db5b11a8.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "3cd53e7b9982de1bfd59",
+    "url": "/static/js/2.2f32b307.chunk.js"
+  },
+  {
+    "revision": "17f647dd5af2a8267932ec527c9dbab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "71dc43b3f7512e5d9575fba19e25835a",
+    "url": "/static/js/2.2f32b307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "ef01275b4f1dfbb5f5cfb0b2014eab26",
+    "url": "/manifest.json"
+  }
+];
